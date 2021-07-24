6790958e7ef18a20082cb6c159a02c16758cc825
Leandro Lima <llimas@outlook.com>
2021-07-24 17:20:28 -0300

    mocks

---
+++ api/rotas/fornecedores/__mocks__/TabelaFornecedor.js
@@ -0,0 +1,27 @@
+module.exports = {
+    listar() {
+        return []
+    },
+    inserir(fornecedor) {
+        return {
+            id: 500,
+            dataCriacao: '10/10/2022',
+            dataAtualizacao: '10/10/2022',
+            versao: 90
+        }
+    },
+    async pegarPorId(id) {
+        return {
+            id: 500,
+            dataCriacao: '10/10/2022',
+            dataAtualizacao: '10/10/2022',
+            versao: 90
+        }
+    },
+    async atualizar(id, dadosParaAtualizar) {
+
+    },
+    async remover(id) {
+
+    }
+}
